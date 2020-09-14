5984b19c1cb01ba8698d3439eda6397f1110bd46
v_dxxjing <v_dxxjing@tencent.com>
2020-09-14 23:04:59 +0800

    test

---
+++ hello/client/main.go
@@ -0,0 +1,25 @@
+package main
+
+import (
+	"context"
+	"fmt"
+	"github.com/micro/go-micro"
+	pb "go_micro/hello/proto"
+)
+
+func main() {
+	service := micro.NewService(
+		micro.Name("Greeter.client"),
+	)
+
+	service.Init()
+
+	greeter := pb.NewGreeterService("Greeter",service.Client())
+
+	rsp,err := greeter.Hello(context.TODO(),&pb.HelloReq{Name:"jdx"})
+	if err != nil{
+		fmt.Println("call hello err:",err)
+		return
+	}
+	fmt.Println(rsp.Greeting)
+}
