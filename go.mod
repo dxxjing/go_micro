16a5503a7b83ebfc043a04f25252b3540b1d0152
v_dxxjing <v_dxxjing@tencent.com>
2020-09-14 23:05:40 +0800

    test

---
+++ go.mod
@@ -0,0 +1,8 @@
+module go_micro
+
+go 1.13
+
+require (
+	github.com/golang/protobuf v1.4.2
+	github.com/micro/go-micro v1.18.0
+)
--- hello/server/main.go
+++ hello/server/main.go
@@ -1,1 +1,36 @@
-package server
+package main
+
+import (
+	"context"
+	"fmt"
+	"github.com/micro/go-micro"
+	pb "go_micro/hello/proto"
+)
+
+//实现greeter.pb.micro.go GreeterHandler 接口
+type GreeterServiceHandler struct {
+}
+
+func (g *GreeterServiceHandler) Hello(ctx context.Context, req *pb.HelloReq, rsp *pb.HelloRsp) error {
+	rsp.Greeting = " 你好 " + req.Name
+	return nil
+}
+
+func main() {
+	//创建新服务
+	service := micro.NewService(
+		micro.Name("Greeter"),
+	)
+	//初始化
+	service.Init()
+	//注册处理器，调用 Greeter 服务接口处理请求
+	if err := pb.RegisterGreeterHandler(service.Server(),new(GreeterServiceHandler));err != nil{
+		fmt.Println("register err:",err)
+		return
+	}
+	//启动服务
+	//默认使用mdns,若要指定etcd 则 go run main.go --registry=etcd
+	if err := service.Run(); err != nil{
+		fmt.Println("run err:",err)
+	}
+}
